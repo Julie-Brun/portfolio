8bb4b0e46e3f5def1c897d5108d48e8694ef78a2
Julie-Brun <brunjulie91@gmail.com>
2020-02-27 11:31:37 +0100

    Add Rocket on Planet

--- src/Components/Planet.js
+++ src/Components/Planet.js
@@ -6,29 +6,36 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 import { createGlowMesh, defaultOptions } from 'three-glow-mesh';
 
-import GLTFPath from '../img/Planet2.glb';
+import GLTFPlanet from '../img/Planet2.glb';
+import GLTFRocket from '../img/Rocket01.glb';
 import '../style.css';
 
 class Planet extends React.Component {
 
     componentDidMount() {
+        // Création de la scène
         var scene = new THREE.Scene();
 
+        // Setup Caméra
         var width = window.innerWidth, height = window.innerHeight;
         var view_angle = 45, aspect = width / height, near = 0.1, far = 1000;
         var camera = new THREE.PerspectiveCamera(view_angle, aspect, near, far);
         camera.position.set(0,150,400);
         scene.add(camera);
 
+        // Setup Renderer
         var renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
         renderer.setSize(window.innerWidth, window.innerHeight);
 
+        // Assigne 
         this.mount.appendChild(renderer.domElement);
 
+        // Setup Lumière
         var light = new THREE.PointLight(0xFFFFFF, 2);
         light.position.set(0, 0, 100);
         camera.add(light);
 
+        // Setup Contrôle de la Planète
         var controls = new OrbitControls(camera, renderer.domElement);
         controls.addEventListener('change', animate); 
 		controls.minDistance = 30;
@@ -36,10 +43,11 @@ class Planet extends React.Component {
         controls.target.set(0, 0, 0);
         controls.update();
         
+        // Génère la Planète
         var planet;
 
         var loader = new GLTFLoader();
-        loader.load( GLTFPath, gltf => {
+        loader.load( GLTFPlanet, gltf => {
             planet = gltf.scene;
             planet.position.set(0, 0, 0);
             planet.scale.set(12, 12, 12);
@@ -47,11 +55,13 @@ class Planet extends React.Component {
             scene.add(planet);
         });
 
+        // Génère une sphère plus petite à l'intérieur de la Planète
         var sphereGeom =  new THREE.SphereGeometry( 59, 32, 16 );
         var darkMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff,  transparent: true, opacity: 0, blending: THREE.AdditiveBlending, side: THREE.BackSide });
         var sphere = new THREE.Mesh(sphereGeom.clone(), darkMaterial);
         sphere.position.set(0, 0, 0);
 
+        // Génère une auréole autour de la petite sphère
         var options = {
             ...defaultOptions,
             backside: true,
@@ -66,11 +76,27 @@ class Planet extends React.Component {
 
         scene.add(sphere);
 
+        // Génère la fusée sur la Planète
+        var rocket, radius = 4.98;
+
+        var loader = new GLTFLoader();
+        loader.load( GLTFRocket, gltf => {
+            rocket = gltf.scene;
+            rocket.position.setFromSphericalCoords(radius + 0.1, THREE.Math.degToRad(40), THREE.Math.degToRad(45));
+            rocket.rotation.set(6.8, 0, 5.8);
+            rocket.scale.set(1, 1, 1);
+            
+            planet.add(rocket);
+        });
+
+
         animate();
 
+        // Fonction pour animer la scène
         function animate() {
             requestAnimationFrame(animate);
 
+            // Fait tourner la Planète
             if (planet) {
                 planet.rotation.y += 0.0005;
             }
